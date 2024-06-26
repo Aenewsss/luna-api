0139c521a7f15d8b1b9888d2f0da26247587a674
aenewsss <aenamartinelli@gmail.com>
2024-06-26 15:37:19 -0300

    test db in python anywhere

--- app/database/database.py
+++ app/database/database.py
@@ -11,8 +11,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    print('Get db test')
+    # db = SessionLocal()
+    # try:
+    #     yield db
+    # finally:
+    #     db.close()
