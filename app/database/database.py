0803165ec44361a0c1fe39e1b545368ffdd3ec05
aenewsss <aenamartinelli@gmail.com>
2024-06-26 15:39:02 -0300

    comment db

--- app/database/database.py
+++ app/database/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 from app import environments
 
-engine = create_engine(environments.DATABASE_URL)
+# engine = create_engine(environments.DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+# Base = declarative_base()
 
 def get_db():
     print('Get db test')
